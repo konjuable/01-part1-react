2449a6dbc1c3811d561a6efe1dd21cd43024a25b
konjuable <juha.konttinen@bc.fi>
2024-02-06 14:39:21 +0200

    Lisätty komponentti Time ja siirretty edellä tehty sisältö sinne. Lisätty komponentti Hello ja sille sisällöksi  otsikko ja kappale. Kutsuttu komponentista App komponentit Time ja Hello (3x).

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-const App = () => {
-  // console.log("Hello from komponentti");
+const Hello = () => {
+  return (
+    <section>
+      <hr></hr>
+      <h1>Greetings</h1>
+
+      <p>Hello world</p>
+    </section>
+  );
+};
+const Time = () => {
   const now = new Date();
   const a = 10;
   const b = 20;
   console.log(now, a + b);
   return (
-    <>
+    <section>
       <p>
         Hello World, it is
         <span> {now.toLocaleDateString()} today,</span>
@@ -14,6 +23,17 @@ const App = () => {
       <p>
         {a} plus {b} is {a + b}
       </p>
+    </section>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Time />
+      <Hello />
+      <Hello />
+      <Hello />
     </>
   );
 };
